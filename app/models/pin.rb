b53fde38fe05452b8697c5c37a8355a75508e969
Evan <evanrulfs@gmail.com>
2014-03-20 22:05:55 -0400

    Removed bucket line from last commit

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,4 @@ class Pin < ActiveRecord::Base
      belongs_to :user
      has_attached_file :image, 
      	:styles => { :medium => "300x300>", :thumb => "100x100>" }
-     	:bucket => 'winteresting',
 end
